c50a4b6371d0c455e9349cef801662ff95cb950c
hjoon <guswns3371@gmail.com>
2023-05-17 00:50:53 +0900

    [section3] 템플릿 메서드 패턴 - 예제2

--- advanced/src/test/java/hello/advanced/trace/template/TemplateMethodTest.java
+++ advanced/src/test/java/hello/advanced/trace/template/TemplateMethodTest.java
@@ -1,5 +1,8 @@
 package hello.advanced.trace.template;
 
+import hello.advanced.trace.template.code.AbstractTemplate;
+import hello.advanced.trace.template.code.SubClassLogic1;
+import hello.advanced.trace.template.code.SubClassLogic2;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 
@@ -39,5 +42,44 @@ public class TemplateMethodTest {
         final long resultTime = endTime - startTime;
 
         log.info("resultTime={}", resultTime);
+    }
+
+
+    /**
+     * 템플릿 메서드 패턴 적용
+     * <p>
+     * 기존에 구현되어있는 구상 클래스를 활용.
+     */
+    @Test
+    void templateMethodV1() {
+        AbstractTemplate template1 = new SubClassLogic1();
+        template1.execute();
+
+        AbstractTemplate template2 = new SubClassLogic2();
+        template2.execute();
+    }
+
+    /**
+     * 익명의 구상 클래스를 구현.
+     */
+    @Test
+    void templateMethodV2() {
+        AbstractTemplate template1 = new AbstractTemplate() {
+            @Override
+            protected void call() {
+                log.info("비즈니스 로직1 실행");
+            }
+        };
+        log.info("클래스 이름1={}", template1.getClass());
+        template1.execute();
+
+        AbstractTemplate template2 = new AbstractTemplate() {
+            @Override
+            protected void call() {
+                log.info("비즈니스 로직2 실행");
+            }
+        };
+        log.info("클래스 이름2={}", template2.getClass());
+        template2.execute();
     }
 }
